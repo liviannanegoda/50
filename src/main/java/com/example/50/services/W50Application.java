a87775cfa8fa1043bdf64fa522dad001d79278c3
liviannanegoda <135698720+liviannanegoda@users.noreply.github.com>
2025-05-29 17:59:24 +0300

    Create W50Application.java

---
+++ src/main/java/com/example/50/services/W50Application.java
@@ -0,0 +1,13 @@
+package com.example.W50;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W50Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W50Application.class, args);
+	}
+
+}
